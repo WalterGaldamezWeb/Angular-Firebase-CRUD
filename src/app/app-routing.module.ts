310caaa9d7872b4229bc16bdca416f3a7865fc26
WalterGaldamezWeb <walter.galdamez.06@gmail.com>
2020-10-24 09:56:23 -0600

    componentes creados

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductosComponent } from './componentes/productos/productos.component';
+import { ProductoComponent } from './componentes/producto/producto.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: '**',
+    pathMatch: 'full',
+    redirectTo: '/productos'
+  },
+  {
+    path: '/productos',
+    component: ProductosComponent
+  },
+  {
+    path: '/producto/:id',
+    component: ProductoComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductoComponent } from './componentes/producto/producto.component';
+import { ProductosComponent } from './componentes/productos/productos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductoComponent,
+    ProductosComponent
   ],
   imports: [
     BrowserModule,
